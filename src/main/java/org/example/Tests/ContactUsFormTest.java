4fe198ed9aa3f85adc5112973b39ad72b6bc0ea4
polughoul <andreystasiuk1488@gmail.com>
2024-06-03 18:06:07 +0200

    small fix

--- src/main/java/org/example/Tests/ContactUsFormTest.java
+++ src/main/java/org/example/Tests/ContactUsFormTest.java
@@ -6,22 +6,26 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import java.nio.file.Paths;
 
 import java.io.FileReader;
 import java.io.Reader;
-import java.util.Iterator;
+import java.nio.file.Paths;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 public class ContactUsFormTest {
     WebDriver driver;
+
+    @FindBy(css = ".fc-button.fc-cta-consent.fc-primary-button")
+    WebElement consentButton;
 
     @FindBy(css = "a[href='/contact_us']")
     WebElement contactUsButton;
@@ -50,9 +54,7 @@ public class ContactUsFormTest {
     @Before
     public void setup() {
         System.setProperty("webdriver.chrome.driver", "chromedriver-win64/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("user-data-dir=C:\\Users\\andre\\AppData\\Local\\Google\\Chrome\\User Data\\default");
-        driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
         PageFactory.initElements(driver, this);
     }
 
@@ -60,69 +62,60 @@ public class ContactUsFormTest {
     public void contactUsFormTest() throws Exception {
         driver.get("http://automationexercise.com");
 
+        consentButton.click();
         contactUsButton.click();
 
         Reader in = new FileReader("src/main/java/data1.csv");
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(in);
-        Iterator<CSVRecord> recordIterator = records.iterator();
 
-        CSVRecord messageRecord = null;
-        CSVRecord nameRecord = null;
-        CSVRecord emailRecord = null;
-        CSVRecord subjectRecord = null;
+        for (CSVRecord record : records) {
+            for (Map.Entry<String, String> entry : record.toMap().entrySet()) {
+                System.out.println(entry.getKey() + ": " + entry.getValue());
+            }
 
-        if (recordIterator.hasNext()) {
-            messageRecord = recordIterator.next();
+            String name = record.get("name").trim();
+            String email = record.get("email").trim();
+            String subject = record.get("subject").trim();
+            String message = record.get("message").trim();
+
+            nameInput.sendKeys(name);
+            Thread.sleep(2000);
+            emailInput.sendKeys(email);
+            Thread.sleep(2000);
+            subjectInput.sendKeys(subject);
+            Thread.sleep(2000);
+            messageInput.sendKeys(message);
+            Thread.sleep(2000);
+
+            String relativePath = "src/main/java/org/example/Tests/test.docx";
+            String absolutePath = Paths.get(relativePath).toAbsolutePath().toString();
+            fileInput.sendKeys(absolutePath);
+
+            String enteredName = nameInput.getAttribute("value");
+            String enteredEmail = emailInput.getAttribute("value");
+            String enteredSubject = subjectInput.getAttribute("value");
+            String enteredMessage = messageInput.getAttribute("value");
+
+            assertEquals(name, enteredName);
+            assertEquals(email, enteredEmail);
+            assertEquals(subject, enteredSubject);
+            assertEquals(message, enteredMessage);
+
+            Thread.sleep(2000);
+
+            submitButton.click();
+            Thread.sleep(2000);
+
+            Alert alert = driver.switchTo().alert();
+            alert.accept();
+
+            assertEquals("Success! Your details have been submitted successfully.", driver.findElement(By.cssSelector(".status.alert.alert-success")).getText());
+
+            homeButton.click();
+
+            Thread.sleep(15000);
+            assertEquals("https://automationexercise.com/", driver.getCurrentUrl());
         }
-
-        if (recordIterator.hasNext()) {
-            nameRecord = recordIterator.next();
-        }
-
-        if (recordIterator.hasNext()) {
-            emailRecord = recordIterator.next();
-        }
-
-        if (recordIterator.hasNext()) {
-            subjectRecord = recordIterator.next();
-        }
-
-        String message = messageRecord != null ? messageRecord.get(0) : "";
-        String name = nameRecord != null ? nameRecord.get(0) : "";
-        String email = emailRecord != null ? emailRecord.get(0) : "";
-        String subject = subjectRecord != null ? subjectRecord.get(0) : "";
-
-        nameInput.sendKeys(name);
-        Thread.sleep(2000);
-        emailInput.sendKeys(email);
-        Thread.sleep(2000);
-        subjectInput.sendKeys(subject);
-        Thread.sleep(2000);
-        messageInput.sendKeys(message);
-        Thread.sleep(2000);
-
-        String relativePath = "src/main/java/org/example/Tests/test.docx";
-        String absolutePath = Paths.get(relativePath).toAbsolutePath().toString();
-        fileInput.sendKeys(absolutePath);
-        Thread.sleep(2000);
-        submitButton.click();
-        Thread.sleep(2000);
-
-        Alert alert = driver.switchTo().alert();
-        alert.accept();
-        String enteredName = nameInput.getAttribute("value");
-        String enteredEmail = emailInput.getAttribute("value");
-        String enteredSubject = subjectInput.getAttribute("value");
-        String enteredMessage = messageInput.getAttribute("value");
-
-        assertEquals(name, enteredName);
-        assertEquals(email, enteredEmail);
-        assertEquals(subject, enteredSubject);
-        assertEquals(message, enteredMessage);
-
-        homeButton.click();
-        Thread.sleep(2000);
-        assertEquals("https://automationexercise.com/", driver.getCurrentUrl());
     }
 
     @After
